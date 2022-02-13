6e782271d23d1fe5c6e0022d3d3b8ae2ff547677
Angelynn <angelynn.trng@gmail.com>
2022-02-13 14:58:09 -0800

    Added constant action type variables

---
+++ client/constants/actionTypes.js
@@ -0,0 +1,17 @@
+/**
+ * ************************************
+ *
+ * @module  actionTypes.js
+ * @description Action Type Constants
+ *
+ * ************************************
+ */
+
+ export const ADD_VOTE = 'ADD_VOTE';
+ export const DELETE_VOTE = 'DELETE_VOTE';VOTE
+ export const ADD_OPTION = 'ADD_OPTION';
+//  export const UPDATE_LOCATION = 'UPDATE_LOCATION';
+ export const RESET_OPTIONS = 'RESET_OPTIONS';
+ export const SPIN_OPTIONS = 'SPIN_OPTIONS';
+ export const SYNC_OPTIONS = 'SYNC_OPTIONS';
+ export const LOAD_OPTIONS = 'LOAD_OPTIONS';
